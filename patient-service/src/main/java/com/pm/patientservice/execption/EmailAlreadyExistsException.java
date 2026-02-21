215451647cacc8ed20d273a7352b726fb5129af9
Mohammadshahzaib007 <shahzaibmohammad07@gmail.com>
2026-02-21 20:10:04 +0530

    add patient service, get all patient, create patient, and global error handler

---
+++ patient-service/src/main/java/com/pm/patientservice/execption/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.pm.patientservice.execption;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
